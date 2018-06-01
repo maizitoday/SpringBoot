ad907aec78757dea36c05772d44e72362381a232
麦子_today <maizi_today@sina.com>
2018-06-01 17:27:01 +0800

    git  版本控制测试

---
+++ gitTest/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author yubo
+ * @version V2.0
+ * @Description:
+ * @date 2018/6/1 下午5:25
+ * @Company: cecsm.com
+ * @Copyright Copyright (c) 2017
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("   git test  ");
+    }
+}
